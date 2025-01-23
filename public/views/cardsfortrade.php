28049cb67690039c7fb9e874f826f80dbde2eb1e
mjkj09 <makejo09@gmail.com>
2025-01-23 23:24:07 +0100

    fix: add missing viewport meta tag for proper responsiveness

--- public/views/cardsfortrade.php
+++ public/views/cardsfortrade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cards for Trade</title>
     <link rel="stylesheet" href="public/css/common.css">
     <link rel="stylesheet" href="public/css/cardlist.css">
